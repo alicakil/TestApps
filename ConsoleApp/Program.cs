50734a7721cfa1a62cdd98649cd756f93c5000ae
Ali ÇAKIL <alicakil78@gmail.com>
2022-02-03 15:18:23 +0300

    fist update.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,1 +1,9 @@
 ﻿Console.WriteLine("This Ali ÇAKIL!\nSenior Software Engineer!");
+
+
+// This app will have some test codes such as Collections.Generics.
+
+List<string> list = new List<string>() { "ali", "veli", "deli" };
+
+foreach (var s in list)
+    Console.WriteLine(s);
